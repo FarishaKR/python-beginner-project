f910f05760d38d12df9b6e61b98ec30dafdd6414
Farisha KR <73113669+FarishaKR@users.noreply.github.com>
2022-06-24 22:40:59 +0530

    new

--- madlib.py
+++ madlib.py
@@ -3,10 +3,10 @@
 #print("subscribe to {}".format(youtuber))
 #print(f"subscribe to {youtuber}")
 
-adj = input("Adjective: ")
+adj1 = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("verb: ")
 famous_person = input("Famous person: ")
 
-madlib = f"computer programming is so {adj}! It makes me so excited all the time because \I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+madlib = f"computer programming is so {adj1}! It makes me so excited all the time because \I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 print(madlib)
